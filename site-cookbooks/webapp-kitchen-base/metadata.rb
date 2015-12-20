0b522cb9c538f06de9c1cb7a1bb09863dc59dacf
Adam Duke <adam.v.duke@gmail.com>
2015-12-20 01:52:08 -0500

    extract a webapp-kitchen-base cookbook

---
+++ site-cookbooks/webapp-kitchen-base/metadata.rb
@@ -0,0 +1,9 @@
+name             'webapp-kitchen-base'
+maintainer       'Adam Duke'
+maintainer_email 'adam.v.duke@gmail.com'
+license          'MIT'
+description      'Installs/Configures webapp-kitchen-base'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
+
+depends 'apt'
